194afa1b78fe5f85bea4715bc4c1c31a0efd8ba7
Hugo Tavares <hugo.tavares@slcu.cam.ac.uk>
2020-02-27 17:46:13 +0000

    remove hard-coded outdir

--- workflow/scripts/getKnownSnps.R
+++ workflow/scripts/getKnownSnps.R
@@ -17,8 +17,6 @@ option_list = list(
 
 opt <- parse_args(OptionParser(option_list=option_list))
 
-opt$outdir <- "data/external/snps/"
-
 if(!dir.exists(opt$outdir)){
   dir.create(opt$outdir)
   setwd(opt$outdir)
