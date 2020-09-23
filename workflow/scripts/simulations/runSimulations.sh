d09f52e0bfe3201e163dabd91008d487ea6ed1f5
Hugo Tavares <hugo.tavares@slcu.cam.ac.uk>
2020-09-23 12:57:32 +0100

    adjust slurm job

--- workflow/scripts/simulations/runSimulations.sh
+++ workflow/scripts/simulations/runSimulations.sh
@@ -6,23 +6,20 @@
 #SBATCH -p skylake
 #SBATCH -c 1
 #SBATCH --mem-per-cpu=1000MB
-#SBATCH -t 00:10:00
-#SBATCH -a 2-121  # to loop through parameter file
+#SBATCH -t 00:15:00
+#SBATCH -a 1-100  # replicate simulations
 
 # This should ideally be incorporated into snakemake...
 
 source activate simupop
 
-# fetch parameters (based on job array number)
-PARAMS=$(head -n $SLURM_ARRAY_TASK_ID data/intermediate/simulations/parameter_list.csv | tail -n 1)
-
-# run 100 iterations
-for i in {1..100}
+# loop through parameters
+while read -r PARAMS
 do
   python workflow/scripts/simulations/single_replicate_simulation.py \
     --n_selected_loci $(echo $PARAMS | cut -d "," -f 1) \
     --selected_effect $(echo $PARAMS | cut -d "," -f 2) \
     --n_adv_alleles $(echo $PARAMS | cut -d "," -f 3) \
     --outdir "data/intermediate/simulations" \
-    --seed "20200916$i"
-done
+    --seed "20200916$SLURM_ARRAY_TASK_ID"
+done < "data/intermediate/simulations/parameter_list.csv"
