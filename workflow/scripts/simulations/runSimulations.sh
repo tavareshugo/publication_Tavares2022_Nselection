e21ed3aeb35601c61be19799b4e8e3a11a5740bf
Hugo Tavares <hm533@cam.ac.uk>
2023-04-05 13:34:38 +0100

    update parameter scan

--- workflow/scripts/simulations/runSimulations.sh
+++ workflow/scripts/simulations/runSimulations.sh
@@ -24,7 +24,7 @@ conda activate simupop
 #     --seed "20200916$SLURM_ARRAY_TASK_ID"
 # done < "data/intermediate/simulations/parameter_list.csv"
 
-for NSEL in 1 3 5 10 20 25 30 35
+for NSEL in 1 3 5 10 20 30 40 50 60
 do
   for EFF in 0.05 0.1 0.2 0.3 0.5 0.7 1
   do
@@ -45,7 +45,7 @@ done
 
 # approximately infinitesimal - 500 loci, i.e. 100 per chromosome
 NSEL="500"
-for EFF in 0.001 0.002 0.005 0.01 0.03 0.05
+for EFF in 0.01 0.03 0.05 0.08 0.1
 do
   for NADV in 1 2 4 8
   do
@@ -60,67 +60,67 @@ do
   done
 done
 
-# 5 loci clumped into one chromosome
-for EFF in 0.1 0.2 0.3 0.5
-do
-  for NADV in 1 2 4 8
-  do
-    # echo "$NSEL-$EFF-$NADV"
-    python workflow/scripts/simulations/single_replicate_simulation.py \
-      --loc_selected_loci "51,101,152,202,253" \
-      --selected_effect $EFF \
-      --n_adv_alleles $NADV \
-      --outdir "data/intermediate/simulations/" \
-      --suffix "5in1-${EFF}-${NADV}-seed${SLURM_ARRAY_TASK_ID}" \
-      --seed "20200916$SLURM_ARRAY_TASK_ID"
+# # 5 loci clumped into one chromosome
+# for EFF in 0.1 0.2 0.3 0.5
+# do
+#   for NADV in 1 2 4 8
+#   do
+#     # echo "$NSEL-$EFF-$NADV"
+#     python workflow/scripts/simulations/single_replicate_simulation.py \
+#       --loc_selected_loci "51,101,152,202,253" \
+#       --selected_effect $EFF \
+#       --n_adv_alleles $NADV \
+#       --outdir "data/intermediate/simulations/" \
+#       --suffix "5in1-${EFF}-${NADV}-seed${SLURM_ARRAY_TASK_ID}" \
+#       --seed "20200916$SLURM_ARRAY_TASK_ID"
       
-  done
-done
+#   done
+# done
 
-# 10 loci clumped into two chromosomes
-for EFF in 0.1 0.2 0.3 0.5
-do
-  for NADV in 1 2 4 8
-  do
-    # echo "$NSEL-$EFF-$NADV"
-    python workflow/scripts/simulations/single_replicate_simulation.py \
-      --loc_selected_loci "51,101,152,202,253,354,404,455,505,556" \
-      --selected_effect $EFF \
-      --n_adv_alleles $NADV \
-      --outdir "data/intermediate/simulations/" \
-      --suffix "10in2-${EFF}-${NADV}-seed${SLURM_ARRAY_TASK_ID}" \
-      --seed "20200916$SLURM_ARRAY_TASK_ID"
-  done
-done
+# # 10 loci clumped into two chromosomes
+# for EFF in 0.1 0.2 0.3 0.5
+# do
+#   for NADV in 1 2 4 8
+#   do
+#     # echo "$NSEL-$EFF-$NADV"
+#     python workflow/scripts/simulations/single_replicate_simulation.py \
+#       --loc_selected_loci "51,101,152,202,253,354,404,455,505,556" \
+#       --selected_effect $EFF \
+#       --n_adv_alleles $NADV \
+#       --outdir "data/intermediate/simulations/" \
+#       --suffix "10in2-${EFF}-${NADV}-seed${SLURM_ARRAY_TASK_ID}" \
+#       --seed "20200916$SLURM_ARRAY_TASK_ID"
+#   done
+# done
 
-# 5 loci closely linked in one chromosome
-for EFF in 0.1 0.2 0.3 0.5
-do
-  for NADV in 1 2 4 8
-  do
-    # echo "$NSEL-$EFF-$NADV"
-    python workflow/scripts/simulations/single_replicate_simulation.py \
-      --loc_selected_loci "26,51,76,101,127" \
-      --selected_effect $EFF \
-      --n_adv_alleles $NADV \
-      --outdir "data/intermediate/simulations/" \
-      --suffix "5in1linked-${EFF}-${NADV}-seed${SLURM_ARRAY_TASK_ID}" \
-      --seed "20200916$SLURM_ARRAY_TASK_ID"
-  done
-done
+# # 5 loci closely linked in one chromosome
+# for EFF in 0.1 0.2 0.3 0.5
+# do
+#   for NADV in 1 2 4 8
+#   do
+#     # echo "$NSEL-$EFF-$NADV"
+#     python workflow/scripts/simulations/single_replicate_simulation.py \
+#       --loc_selected_loci "26,51,76,101,127" \
+#       --selected_effect $EFF \
+#       --n_adv_alleles $NADV \
+#       --outdir "data/intermediate/simulations/" \
+#       --suffix "5in1linked-${EFF}-${NADV}-seed${SLURM_ARRAY_TASK_ID}" \
+#       --seed "20200916$SLURM_ARRAY_TASK_ID"
+#   done
+# done
 
-# 5 loci in one chromosome and 1 in each of the others
-for EFF in 0.1 0.2 0.3 0.5
-do
-  for NADV in 1 2 4 8
-  do
-    # echo "$NSEL-$EFF-$NADV"
-    python workflow/scripts/simulations/single_replicate_simulation.py \
-      --loc_selected_loci "51,101,152,202,253,455,650,883,1067" \
-      --selected_effect $EFF \
-      --n_adv_alleles $NADV \
-      --outdir "data/intermediate/simulations/" \
-      --suffix "5in1plus4-${EFF}-${NADV}-seed${SLURM_ARRAY_TASK_ID}" \
-      --seed "20200916$SLURM_ARRAY_TASK_ID"
-  done
-done
+# # 5 loci in one chromosome and 1 in each of the others
+# for EFF in 0.1 0.2 0.3 0.5
+# do
+#   for NADV in 1 2 4 8
+#   do
+#     # echo "$NSEL-$EFF-$NADV"
+#     python workflow/scripts/simulations/single_replicate_simulation.py \
+#       --loc_selected_loci "51,101,152,202,253,455,650,883,1067" \
+#       --selected_effect $EFF \
+#       --n_adv_alleles $NADV \
+#       --outdir "data/intermediate/simulations/" \
+#       --suffix "5in1plus4-${EFF}-${NADV}-seed${SLURM_ARRAY_TASK_ID}" \
+#       --seed "20200916$SLURM_ARRAY_TASK_ID"
+#   done
+# done
